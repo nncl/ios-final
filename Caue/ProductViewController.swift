c6cabd977934a45020faae8d1b05b68f169e98f0
= <nncl@live.com>
2017-04-20 22:04:28 -0300

    Picking photo up from device through either camera or library

--- Caue/ProductViewController.swift
+++ Caue/ProductViewController.swift
@@ -12,15 +12,46 @@ import CoreData
 class ProductViewController: UIViewController {
     
     // MARK: - Variables
+    var smallImage: UIImage!
     var dataSource: [State] = []
     var product: Product!
     var pickerView: UIPickerView!
     @IBOutlet weak var tfName: UITextField!
-    @IBOutlet weak var ivImage: UIImageView!
     @IBOutlet weak var tfPrice: UITextField!
     @IBOutlet weak var swCard: UISwitch!
     @IBOutlet weak var btAddUpdate: UIButton!
     @IBOutlet weak var tfState: UITextField!
+    @IBOutlet weak var ivPoster: UIImageView!
+    
+    func selectPicture(sourceType: UIImagePickerControllerSourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func addPoster(_ sender: UIButton) {
+        print("Add poster")
+        let alert = UIAlertController(title: "Selecionar poster", message: "De onde você quer escolher o poster", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Câmera", style: .default) { (action: UIAlertAction) in
+                self.selectPicture(sourceType: .camera)
+            }
+            alert.addAction(cameraAction)
+        }
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action: UIAlertAction) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        
+        present(alert, animated: true, completion: nil)
+    }
     
     // MARK: - Functions/Methods
     func cancel() {
@@ -162,3 +193,19 @@ extension ProductViewController: UIPickerViewDataSource {
     }
 }
 
+extension ProductViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        // Reduzir imagem
+        let smallSize = CGSize(width: 300, height: 280)
+        UIGraphicsBeginImageContext(smallSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: smallSize.width, height: smallSize.height))
+        smallImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        ivPoster.image = smallImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
